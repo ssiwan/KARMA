6202ad3bc5a6e9b289a62be0b7a0c03318f550aa
akebede <akebede@stanfieldsystems.com>
2018-02-23 16:49:43 -0800

    Fix for ignoring change password bdd test

--- KarmaBDD/src/test/java/com/stanfield/karma/bdd/steps/ChangePasswordStepDefinitions.java
+++ KarmaBDD/src/test/java/com/stanfield/karma/bdd/steps/ChangePasswordStepDefinitions.java
@@ -2,10 +2,10 @@ package com.stanfield.karma.bdd.steps;
 
 import com.stanfield.karma.bdd.steps.serenity.*;
 
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
+
 
 public class ChangePasswordStepDefinitions {
 
--- KarmaBDD/src/test/resources/com/stanfield/karma/bdd/features/change_password.feature
+++ KarmaBDD/src/test/resources/com/stanfield/karma/bdd/features/change_password.feature
@@ -3,7 +3,7 @@ Feature: Change user password
   As a user, when I log into the application, I can change my password 
   so that the password is meaningful and secure
 
-  ~@ignore
+  @ignore
   Scenario: Change user password
     Given I am logged in 
    	When I click on the Account button
